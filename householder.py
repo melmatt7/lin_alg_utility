970a7c34da77cbab38ca6bfe6e8dff8d722702a5
melmatt7 <mel.matt007@gmail.com>
2020-11-26 15:03:27 -0800

    hw2

--- householder.py
+++ householder.py
@@ -4,19 +4,20 @@ import scipy.linalg as la
 import matplotlib.pyplot as plt
 
 
-A = np.array([[1,0,4],[1,2,2],[1,1,2],[-1,2,4]])
+A = np.array([[1,0,1],[0,1,1],[2,2,0],[1,2,2]])
 
 e = np.identity(A.shape[0])
 print(A.shape)
 H = []
 
-for col_val in range(0,A.shape[1]):
-    alpha = -A[col_val,col_val]/np.abs(A[col_val,col_val])*(la.norm(A[:,col_val]))
-    print(alpha)
-    u = A[:,col_val] - alpha*e[:,col_val]
-    print(u)
-    a = np.matrix(u)
-    H.append(e - (2/(la.norm(u))**2)*(np.matrix(u).T*np.matrix(u)))
+col_val = 1
+
+alpha = -A[col_val,col_val]/np.abs(A[col_val,col_val])*(la.norm(A[:,col_val]))
+print(alpha)
+u = A[:,col_val] - alpha*e[:,col_val]
+print(u)
+a = np.matrix(u)
+H.append(e - (2/(la.norm(u))**2)*(np.matrix(u).T*np.matrix(u)))
 
 print(H)
 
