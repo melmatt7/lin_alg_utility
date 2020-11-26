7dd876c145a7899e637ad2547e80288488265a10
Melvin Mathews <mel.matt007@gmail.com>
2020-11-26 15:39:07 -0800

    householder

--- fundamental_subspace.py
+++ fundamental_subspace.py
@@ -3,13 +3,13 @@ from sympy import *
 
 # The four fundamental subspaces of a matrix
 
-A = Matrix([[1, 2, -1, 1], [2, 1, -1, 4], [1, -4, 1, 5]])
+A = Matrix([[1, 0, -4, -3], [-2, 1, 13, 5], [0, 1, 5, -1]])
 
 pprint(A)
 
 print('Column space of A =')
-for col in A.rref()[1]:
-    print([A[:, col]])
+for col in A.T.rref()[1]:
+    print([A.T[:, col]])
 print('Null space of A =')
 pprint(A.nullspace())
 print('Row space of A =')
--- householder.py
+++ householder.py
@@ -4,22 +4,17 @@ import scipy.linalg as la
 import matplotlib.pyplot as plt
 
 
-A = np.array([[1,0,4],[1,2,2],[1,1,2],[-1,2,4]])
+A = np.array([[2,0,1],[4,-2,3],[0,2,-2],[5,1,0]])
+col_val = 1
 
 e = np.identity(A.shape[0])
-print(A.shape)
-H = []
 
-for col_val in range(0,A.shape[1]):
-    alpha = -A[col_val,col_val]/np.abs(A[col_val,col_val])*(la.norm(A[:,col_val]))
-    print(alpha)
-    u = A[:,col_val] - alpha*e[:,col_val]
-    print(u)
-    a = np.matrix(u)
-    H.append(e - (2/(la.norm(u))**2)*(np.matrix(u).T*np.matrix(u)))
+alpha = -A[col_val,col_val]/np.abs(A[col_val,col_val])*(la.norm(A[:,col_val]))
+print(alpha)
+u = A[:,col_val] - alpha*e[:,col_val]
+print(u)
+a = np.matrix(u)
+H = e - 2/(la.norm(u))**2*(np.matrix(u).T*np.matrix(u))
 
 print(H)
-
-print(H[0]*A)
-print(H[1]*H[0]*A)
-
+print(H*A)
